93cc969658754e559f9a16ec88f8869f457c5775
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2020-06-10 11:30:42 +0900

    add json

--- df2csv.py
+++ df2csv.py
@@ -13,9 +13,11 @@ def df2csv():
 
     path_n = "T:\\ProgramFilesT\\pleiades\\workspace\\node225\\nt1570.csv"
     path_c = "T:\\ProgramFilesT\\pleiades\\workspace\\nikkei\\Debug\\N225BP.csv"
+    path_p = "C:\\Users\\sadaco\\Documents\\GitHub\\sakata\\json\\n225in.json" #
 
     df.to_csv(path_n, header=True, index=False)  # nodejs
     df.to_csv(path_c, header=False, index=False, line_terminator="\n")  # C lang
+    df.to_json(path_p, orient="records") # python
     print("Done df2csv")
 
 
