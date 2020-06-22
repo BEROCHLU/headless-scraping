b0abb9a45fd76f2948c79eafc1cbcb546a60a482
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2020-06-22 13:04:57 +0900

    del csv

--- df2csv.py
+++ df2csv.py
@@ -11,13 +11,11 @@ def df2csv():
     df = df.dropna(subset=["upro", "fxy", "t1570"])  # 欠損値(NaN)を除外
     df = df.loc[:, ["date", "upro", "fxy", "t1570"]]  # 指定列だけ
 
-    path_n = "T:\\ProgramFilesT\\pleiades\\workspace\\node225\\nt1570.csv"
     path_c = "T:\\ProgramFilesT\\pleiades\\workspace\\nikkei\\Debug\\N225BP.csv"
-    path_p = "C:\\Users\\sadaco\\Documents\\GitHub\\sakata\\json\\n225in.json" #
+    path_p = "C:\\Users\\sadaco\\Documents\\GitHub\\sakata\\json\\n225in.json"
 
-    df.to_csv(path_n, header=True, index=False)  # nodejs
     df.to_csv(path_c, header=False, index=False, line_terminator="\n")  # C lang
-    df.to_json(path_p, orient="records") # python
+    df.to_json(path_p, orient="records", indent=4) # nodejs python
     print("Done df2csv")
 
 
