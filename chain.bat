f9b1977554800f5ca18d830887885f9827776efb
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2021-03-09 09:55:33 +0900

    bat

--- chain.bat
+++ chain.bat
@@ -1,1 +1,1 @@
-python headless.py & python openpycel.py & python df2csv.py & python delete3files.py
+python headless.py && python openpycel.py && python df2csv.py && python delete3files.py
---
+++ start.bat
@@ -0,0 +1,1 @@
+python headless.py && python openpycel.py && python df2csv.py && python delete3files.py && cd ../sakata && npm run o2
