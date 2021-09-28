2ec22c9f22d25c99b0776e321caa8f64abeec30b
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2021-09-28 10:50:24 +0900

    .

--- webscraping.py
+++ webscraping.py
@@ -14,7 +14,7 @@ str_k = b"aHR0cHM6Ly9rYWJ1dGFuLmpwL3N0b2NrL2thYnVrYT9jb2RlPTEzMjEmYXNoaT1kYXkmcG
 str_m = b"aHR0cHM6Ly9meC5taW5rYWJ1LmpwL2FwaS92Mi9iYXIvRVVSVVNEL2RhaWx5Lmpzb24="
 str_ua = b"TW96aWxsYS81LjAgKE1hY2ludG9zaDsgSW50ZWwgTWFjIE9TIFggMTFfNikgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzk0LjAuNDYwNi41NCBTYWZhcmkvNTM3LjM2"
 # path
-output_folder = "../sakata2/csv"
+output_folder = "../sakata/csv"
 # set timezone
 edt = tz.gettz("America/New_York")
 # lambda
